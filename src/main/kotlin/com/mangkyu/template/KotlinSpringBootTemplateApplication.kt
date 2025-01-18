04bf3e91ae818f5d5525a762f93181a265f2e7ed
조민규 <whalsrb1226@naver.com>
2025-01-18 23:06:21 +0900

    Initial commit

---
+++ src/main/kotlin/com/mangkyu/template/KotlinSpringBootTemplateApplication.kt
@@ -0,0 +1,11 @@
+package com.mangkyu.template
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringBootTemplateApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSpringBootTemplateApplication>(*args)
+}
---
+++ src/test/kotlin/com/mangkyu/template/KotlinSpringBootTemplateApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mangkyu.template
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringBootTemplateApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
